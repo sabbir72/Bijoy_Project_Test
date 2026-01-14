cefea7a80ce2b5f0398976ae3db014d9374e3c27
sabbir72 <sabbircse72@gmail.com>
2026-01-14 23:21:58 +0600

    popup sms check done

--- Log_Sing/Signup.py
+++ Log_Sing/Signup.py
@@ -4,30 +4,27 @@ import re
 from conftest import signup_page
 import pytest
 
-def test_signup_Doc(signup_page):
-   Create_acc= signup_page.get_by_role('link', name='Create an account')
-   Create_acc.click()
+# def test_signup_Doc(signup_page):
+#    Create_acc= signup_page.get_by_role('link', name='Create an account')
+#    Create_acc.click()
 
-   Firs_Name= signup_page.get_by_role('textbox', name='First Name')
-   Firs_Name.fill("Sabbir")
-   Last_Nmae=signup_page.get_by_role('textbox', name='Last Name')
-   Last_Nmae.fill("Hossain")
+#    Firs_Name= signup_page.get_by_role('textbox', name='First Name')
+#    Firs_Name.fill("Sabbir")
+#    Last_Nmae=signup_page.get_by_role('textbox', name='Last Name')
+#    Last_Nmae.fill("Hossain")
 
-   Email_phone = signup_page.get_by_role('textbox', name='Email/Phone')
-   Email_phone.fill(os.getenv("NUMBER"))
-   Email_phone.press("Tab")   # validation trigger
+#    Email_phone = signup_page.get_by_role('textbox', name='Email/Phone')
+#    Email_phone.fill(os.getenv("NUMBER"))
+#    Email_phone.press("Tab")   # validation trigger
 
-   Validation_text = signup_page.get_by_text(
-        "Email/Phone is invalid",
-        exact=True
-    )
+#    Validation_text = signup_page.get_by_text("Email/Phone is invalid",exact=True)
 
-   if Validation_text.is_visible():
-        print("❌ Validation Failed: Email/Phone is invalid")
-   else:
-        print("✅ Successfully passed")
+#    if Validation_text.is_visible():
+#         print("❌ Validation Failed: Email/Phone is invalid")
+#    else:
+#         print("✅ Successfully passed")
 
-   signup_page.screenshot(path="Signup_page.png", full_page=True)
+#    signup_page.screenshot(path="Signup_page.png", full_page=True)
 
    #  test cas 02 all fields check===================================================
    #  ===============================================================================
@@ -53,12 +50,17 @@ def test_signup_All_fields_check(signup_page):
 
    Submit_Btn=signup_page.locator('button[type="submit"]')
    Submit_Btn.click()
+   # signup_page.wait_for_load_state("networkidle")
+    
+   signup_page.wait_for_timeout(5000)
 
+   PopUp_sms=signup_page.locator('.ant-notification-notice-message')
+   PopUp_sms.is_visible()
    
-   PopUp=signup_page.get_by_text("Account created successfully. Verify your email/phone to continue.", exact=True)
-   if PopUp.is_visible():
-      print("✅ Signup Successful: Account created successfully popup is visible")
-   else:
-      print("❌  popup is not visible")   
+   print("✅ Signup Successful: ",PopUp_sms.inner_text())
+   
+   signup_page.screenshot(path="Signup_page2.png", full_page=True)
+
+    
 
   
